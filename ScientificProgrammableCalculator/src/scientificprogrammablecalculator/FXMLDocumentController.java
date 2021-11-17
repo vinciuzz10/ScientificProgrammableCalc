098161a8d6970e64d6adcda3f169aa5a27cffbd9
Vinciuzz10 <>
2021-11-17 10:49:43 +0100

    Empty project for setupping tools.

--- ScientificProgrammableCalculator/src/scientificprogrammablecalculator/FXMLDocumentController.java
+++ ScientificProgrammableCalculator/src/scientificprogrammablecalculator/FXMLDocumentController.java
@@ -19,7 +19,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Final flash!");
+        label.setText("FProva a fare la pull!");
     }
     
     @Override
